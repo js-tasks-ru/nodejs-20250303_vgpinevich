668299520369e7dcb6145f866d5a073a1e2294e7
vgpinevich <vgpinevich@avito.ru>
2025-03-30 23:25:13 +0300

    [04-request-lifecycle] solved task

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,46 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import {
+  ArgumentsHost,
+  ExceptionFilter,
+  HttpStatus,
+  Logger,
+} from "@nestjs/common";
+import { Response } from "express";
+import { appendFileSync } from "node:fs";
+
+const LOG_FILE = "errors.log"
 
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  private readonly logger = new Logger(HttpErrorFilter.name);
+
+  private logToFile(message: string): void {
+    try {
+      appendFileSync(LOG_FILE, message + "\n");
+    } catch (e) {
+      this.logger.log(`appendFileSync exception: ${e}`);
+    }
+  }
+
+  private getStatusCode(exception: Error): number {
+    if ("status" in exception && typeof exception.status === "number") {
+      return exception.status;
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+
+  catch(exception: Error, host: ArgumentsHost) {
+    const { message } = exception;
+    const statusCode = this.getStatusCode(exception);
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const timestamp = new Date().toISOString();
+
+    this.logToFile(`[${timestamp}] ${statusCode} - ${message}`);
+
+    response.status(statusCode).json({
+      statusCode,
+      message,
+      timestamp,
+    });
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
+++ 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from "@nestjs/common";
+import { UserRoles, USER_ROLE_HEADER } from "./roles.guard.model";
 
 export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {}
+  canActivate(context: ExecutionContext) {
+    const req = context.switchToHttp().getRequest<Request>();
+
+    const role = req.headers[USER_ROLE_HEADER];
+
+    if (role !== UserRoles.admin) {
+      throw new ForbiddenException(
+        `Доступ запрещён: требуется роль ${UserRoles.admin}`,
+      );
+    }
+
+    return true;
+  }
 }
